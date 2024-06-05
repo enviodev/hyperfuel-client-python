c1f7ba57533409fa0e0f4c65d1a5bc86b0e22090
jossduff <jduff360@gmail.com>
2024-06-05 12:21:25 -0400

    update comments

--- examples/predicate-root.py
+++ examples/predicate-root.py
@@ -1,6 +1,6 @@
 # This example returns `input` field data from an input
-# where the `owner` field matches the predicate root 0x48a0f31c78e1c837ff6a885785ceb7c2090f86ed93db3ed2d8821d13739fe981
-# `owner` is ["The owning address or predicate root."](https://docs.fuel.network/docs/beta-4/graphql/reference/objects/#inputcoin) of an InputCoin Input type
+# where the `owner` field matches the predicate root 0x94a8e322ff02baeb1d625e83dadf5ec88870ac801da370d4b15bbd5f0af01169
+# `owner` is ["The owning address or predicate root."](https://docs.fuel.network/docs/graphql/reference/objects/#inputcoin) of an InputCoin Input type
 
 import hyperfuel
 from hyperfuel import InputField
@@ -14,11 +14,13 @@ async def main():
         from_block = 0,
         # if to_block is not set, query runs to the end of the chain
         to_block=1427625,
+        # load inputs that have `owner` = 0x94a8e322ff02baeb1d625e83dadf5ec88870ac801da370d4b15bbd5f0af01169
         inputs=[
             hyperfuel.InputSelection(
                 owner=["0x94a8e322ff02baeb1d625e83dadf5ec88870ac801da370d4b15bbd5f0af01169"]
             )
         ],
+        # fields we want returned from loaded inputs
         field_selection=hyperfuel.FieldSelection(
             input=[
                 InputField.TX_ID,
--- examples/simple-logs.py
+++ examples/simple-logs.py
@@ -1,13 +1,16 @@
-# This example will simply fetch the logs from a vector of contracts over a specified block range
+# This example will simply fetch the logs from a vector of contracts over a specified block range.
+# returns all log data necessary for fuel's decoder.
 import hyperfuel
 import asyncio
 
 async def main():
     client = hyperfuel.HyperfuelClient()
 
+    # contract(s) we want logs from
     contracts = ["0x4a2ce054e3e94155f7092f7365b212f7f45105b74819c623744ebcc5d065c6ac"]
 
-    logs = await client.preset_query_get_logs(contracts, 0, to_block=None)
+    # get logs from blocks 0 (inclusive) to 1627509 (exclusive)
+    logs = await client.preset_query_get_logs(contracts, 0, 1627509)
 
     print("number of logs: " + str(len(logs.data)))
     print("logs: " + str(logs.data))
